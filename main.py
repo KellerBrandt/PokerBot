d04252698aec91db3f00906c057728f987753aea
keller <kellerbrandt@berkeley.edu>
2024-09-20 15:17:39 -0700

    started on poker bot

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+
+class Poker:
+
+    FRESH_DECK = ('C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK', 'CA',
+                  'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK', 'DA',
+                  'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK', 'HA',
+                  'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK', 'SA')
+    
+
+    def __init__(self):
+        self.shuffle_deck()
+
+    def shuffle_deck(self, unshuffled_deck=FRESH_DECK):
+        temporary_deck = list(unshuffled_deck)
+        shuffled_deck = []
+        while (temporary_deck) :
+            rand = random.randint(0, len(temporary_deck) - 1)
+            shuffled_deck.append(temporary_deck[rand])
+            temporary_deck.pop(rand)
+        self.current_deck = shuffled_deck
+
+poker = Poker()
+print(poker.current_deck)
+poker.shuffle_deck(poker.current_deck)
+print(poker.current_deck)
+poker.shuffle_deck(poker.current_deck)
+print(poker.current_deck)
