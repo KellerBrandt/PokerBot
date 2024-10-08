7bf6b6609f7b46d7e802ed65edaaeea5aaf39d7c
Keller Brandt <103919048+KellerBrandt@users.noreply.github.com>
2024-10-07 23:37:56 -0700

    redid kuhnpoker

--- kuhnPoker.cpp
+++ kuhnPoker.cpp
@@ -1,82 +1,78 @@
 #include <iostream>
-#include <stdio.h>
-#include <utility>
+#include <vector>
+
+class Player {
+private:
+  int stack;
+  int bet;
+
+public:
+  Player() {}
+  void playTurn(int round) {
+    switch (round) {
+    case 0:
+      printf("Test0");
+      break;
+    case 1:
+      printf("Test1");
+    case 2:
+      printf("Test2");
+    }
+  }
+};
+
+class PokerBot : public Player {};
 
 class KuhnPoker {
-  private:
-	struct Card {
-		Card(char s, int r) : suit(s), rank(r) {}
-		char suit;
-		int rank;
-	};
+private:
+  std::vector<Player> players;
+  int pot; // total amount in the pot
+  int gameState;
+  /*
+  0: ante round
+  1: first round
+  2: second round
+  ...
+  */
+  bool hasBet; // whether or not a player has bet this round
+  bool playingGame;
 
-	class Player {
-	  public:
-		Player() {
-		}
-	};
+public:
+  KuhnPoker() {
+    pot = 0;
+    gameState = 0;
+    hasBet = false;
+    playingGame = true;
+    players.push_back(Player());
+    players.push_back(Player());
+  }
 
-	const Card *freshDeck[52] = {new Card('C', 2), new Card('C', 3), new Card('C', 4), new Card('C', 5), new Card('C', 6), new Card('C', 7), new Card('C', 8), new Card('C', 9), new Card('C', 10), new Card('C', 11), new Card('C', 12), new Card('C', 13), new Card('C', 14),
-								 new Card('D', 2), new Card('D', 3), new Card('D', 4), new Card('D', 5), new Card('D', 6), new Card('D', 7), new Card('D', 8), new Card('D', 9), new Card('D', 10), new Card('D', 11), new Card('D', 12), new Card('D', 13), new Card('D', 14),
-								 new Card('H', 2), new Card('H', 3), new Card('H', 4), new Card('H', 5), new Card('H', 6), new Card('H', 7), new Card('H', 8), new Card('H', 9), new Card('H', 10), new Card('H', 11), new Card('H', 12), new Card('H', 13), new Card('H', 14),
-								 new Card('S', 2), new Card('S', 3), new Card('S', 4), new Card('S', 5), new Card('S', 6), new Card('S', 7), new Card('S', 8), new Card('S', 9), new Card('S', 10), new Card('S', 11), new Card('S', 12), new Card('S', 13), new Card('S', 14)};
-	Player *players[2];
+  ~KuhnPoker() {}
 
-  public:
-	KuhnPoker() {
-		players[0] = new Player();
-		players[1] = new Player();
-	}
+  void playGame() {
+    while (playingGame) {
+      for (Player p : players) {
+        p.playTurn(0);
+      }
+      if (gameState == 2) {
+        playingGame = false;
+      }
 
-	~KuhnPoker() {
-		// memory deallocation for freshDeck
-		for (int i = 0; i < sizeof(freshDeck); ++i) {
-			delete freshDeck[i];
-			freshDeck[i] = nullptr;
-		}
-		delete[] freshDeck;
-		//memory deallocation for players
-		delete players[0];
-		delete players[1];
-		delete[] players;
-	}
+      ++gameState;
+    }
+  }
 
-	void playGame() {
-		ante();
-		dealCards();
-		/*
-		player 1 then checks or bets 1
-		if p1 checks:
-			if p2 checks:
-				cards are shown
-			if p2 bets:
-				if p1 matches/calls:
-					cards are shown
-				if p1 folds:
-					p2 gets the pot
-
-		if p1 bets 1:
-			if p2 folds:
-				p1 wins
-			if p2 calls:
-				cards are shown
-		*/
-	}
-
-	int scoreHand() {
-	}
-
-	std::pair<int, Card *[]> findBestHand(Card hand[], Card communityCards[]) {
-	}
-
-	void ante() {
-	}
-
-	void dealCards() {
-	}
+  void resetGame() {
+    pot = 0;
+    gameState = 0;
+    hasBet = false;
+    playingGame = true;
+  }
 };
 
 int main() {
-	printf("Test");
-	return 0;
+  printf("Test");
+  KuhnPoker kPokerGame = KuhnPoker();
+  kPokerGame.playGame();
+  return 0;
 }
