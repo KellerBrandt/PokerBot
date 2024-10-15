f53ffd89c207fb40cff33e3fd21924b29387da8b
Keller Brandt <103919048+KellerBrandt@users.noreply.github.com>
2024-10-14 21:46:03 -0700

    upload

--- kuhnPoker.cpp
+++ kuhnPoker.cpp
@@ -6,23 +6,41 @@
 
 class KuhnPoker {
   private:
+	class Node {
+	  private:
+	  public:
+		Node *parent, *left, *right; // left is pass, right is bet
+		std::vector<int> regretSum, strategySum;
+		int history; // 0 means you start, 1 means they pass, 2 means they bet
+		int card;
+
+		Node(int card, int history) {
+			this->card = card;
+			this->history = history;
+			regretSum = {0, 0};
+			strategySum = {0, 0};
+		}
+	};
+
   public:
 	std::vector<int> cards;	  // 0: jack, 1: queen, 2: king
 	std::vector<int> actions; // 0: pass, 1: bet
-  int actionCount;
+	std::mt19937 gen;
+	std::unordered_map<int, Node> playerOneNodes, playerTwoNodes;
+	int actionCount;
 
 	KuhnPoker() {
 		cards = {0, 1, 2};
 		actions = {0, 1};
-    actionCount = 2;
+		gen = std::mt19937(std::random_device()());
+		actionCount = 2;
 	}
 
 	std::vector<double> getStrategy(std::vector<int> regretSum) {
 	}
 
 	int getAction(std::vector<double> strategy) {
-		//std::mt19937 gen(std::random_device()()); // optimization to be had here
-		//return std::discrete_distribution<>(strategy.begin(), strategy.end())(gen);
+		return std::discrete_distribution<>(strategy.begin(), strategy.end())(gen);
 	}
 
 	int getReward() {
@@ -32,29 +50,18 @@ class KuhnPoker {
 		return cards[cardInd];
 	}
 
-  void cfr() {
-
-  }
-
-	void train(int iterations) {
-    std::vector<double> strategy, oppStrategy;
-    for (int i = 0; i < iterations; ++i) {
-
-    }
+	int cfr(Node &n) {
+		
 	}
 
-	class Node {
-	  private:
-	  public:
-		std::vector<int> regretSum, strategySum;
-    int history; //0 means they checked(passed), 1 means they bet
-    int card;
+	void playGame() {
+		
+	}
 
-		Node(int card, int history) {
-      this.card = card;
-      this.history = history;
-      regretSum = {0, 0};
-      strategySum = {0, 0};
-    }
-	};
+	void train(int iterations) {
+		std::vector<double> strategy, oppStrategy;
+		for (int i = 0; i < iterations; ++i) {
+			playGame
+		}
+	}
 };
