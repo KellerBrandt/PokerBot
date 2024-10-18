49a8519d5b718832116d28d50060a159f1b3dd42
Keller Brandt <103919048+KellerBrandt@users.noreply.github.com>
2024-10-18 12:18:26 -0700

    worked on printing results

--- kuhnPoker.cpp
+++ kuhnPoker.cpp
@@ -5,25 +5,40 @@
 #include <unordered_map>
 #include <vector>
 
+class Node {
+  private:
+  public:
+	std::vector<int> regretSum, strategySum;
+	int history; // 1: pass, 2: bet, 0: nothing
+	int card;
+	int key;
+
+	Node(int card, int history, int key) : card(card), history(history), key(key) {
+		regretSum = {0, 0};
+		strategySum = {0, 0};
+	}
+
+	Node() : card(-1), history(-1) {} // Default constructor
+
+	int getPlayer() {
+		int temp = key;
+		while (temp / 10 > 0) {
+			temp /= 10;
+		}
+		return temp;
+	}
+
+	std::string toString() {
+		std::string returnString;
+		returnString += std::to_string(card) + " ";
+		returnString += std::to_string(history) + " ";
+		returnString += std::to_string(strategySum[0]) + " " + std::to_string(strategySum[1]);
+		return returnString;
+	}
+};
+
 class KuhnPoker {
   private:
-	class Node {
-	  private:
-	  public:
-		Node *parent, *left, *right; // left is pass, right is bet
-		std::vector<int> regretSum, strategySum;
-		// history goes right to left, so the rightmost value is the first action to occur
-		int history; // 1: pass, 2: bet, 0:nothing
-		int card;
-
-		Node(int card, int history) : card(card), history(history) {
-			regretSum = {0, 0};
-			strategySum = {0, 0};
-		}
-
-		Node() : card(-1), history(-1) {} // Default constructor
-	};
-
   public:
 	std::vector<int> cards;	  // 0: jack, 1: queen, 2: king
 	std::vector<int> actions; // 1: pass, 2: bet
@@ -37,9 +52,9 @@ class KuhnPoker {
 		gen = std::mt19937(std::random_device()());
 		actionCount = 2;
 		// add the first three nodes
-		nodes[getKey(0, 0, 0)] = Node(0, 0);
-		nodes[getKey(0, 1, 0)] = Node(1, 0);
-		nodes[getKey(0, 2, 0)] = Node(2, 0);
+		nodes[getKey(0, 0, 0)] = Node(0, 0, getKey(0, 0, 0));
+		nodes[getKey(0, 1, 0)] = Node(1, 0, getKey(0, 1, 0));
+		nodes[getKey(0, 2, 0)] = Node(2, 0, getKey(0, 2, 0));
 	}
 
 	std::vector<double> getStrategy(std::vector<int> regretSum) {
@@ -83,22 +98,23 @@ class KuhnPoker {
 
 	void playGame() {
 		shuffleDeck();
-		Node currentNode = nodes[getKey(0, cards[0], 0)];
+		Node currentNode;
 		std::vector<double> currentPlayerStrategy;
 		int currentPlayerAction;
 		int history = 0;
 		int currentPlayer = 0; // 0 is p1, 1 is p2
 
-		while (!isEndNode(currentNode)) {
+		while (!(history == 11 || history > 12)) {
+			int key = getKey(currentPlayer, cards[currentPlayer], history);
+			if (nodes[key].card == -1) {
+				nodes[key] = Node(cards[currentPlayer - 1], history, key);
+			}
+			currentNode = nodes[key];
 			currentPlayerStrategy = getStrategy(currentNode.regretSum);
 			currentPlayerAction = getAction(currentPlayerStrategy);
 			history = history * 10 + currentPlayerAction;
 
 			currentPlayer = !currentPlayer;
-			if (nodes[getKey(currentPlayer, cards[currentPlayer], history)].card == -1) {
-				nodes[getKey(currentPlayer, cards[currentPlayer], history)] = Node(cards[currentPlayer - 1], history);
-			}
-			currentNode = nodes[getKey(currentPlayer, cards[currentPlayer], history)];
 		}
 	}
 
@@ -113,21 +129,39 @@ class KuhnPoker {
 		int exponent = (std::log10(history)); // might be a source of time inneficiency
 		return (player + 1) * std::pow(10, exponent + 2) + card * std::pow(10, exponent + 1) + history;
 	}
-
-	bool isEndNode(Node n) {
-		// may be a source of error
-		if (n.history == 11 || n.history > 12) {
-			return true;
-		}
-		return false;
-	}
 };
+
+bool comp(Node a, Node b) {
+	return a.getPlayer() < b.getPlayer();
+}
 
 int main() {
 	KuhnPoker kp;
-	for (int i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10000; ++i) {
 		kp.playGame();
 	}
+	int count = 0;
+	std::vector<Node> nodes;
+	for (auto &pair : kp.nodes) {
+		nodes.push_back(pair.second);
+		++count;
+	}
+	std::sort(nodes.begin(), nodes.end(), comp);
+
+	std::cout << "Player 1:\n";
+	for (int i = 1; i < 13; ++i) {
+		std::cout << nodes[i].key << " " << nodes[i].toString() << "\n";
+	}
+	std::cout << "\n";
+
+	std::cout << "Player 2:\n";
+	for (int i = 13; i < 25; ++i) {
+		std::cout << nodes[i].key << " " << nodes[i].toString() << "\n";
+	}
+	std::cout << "\n";
+
+	std::cout << "Count: " << count << "\n";
+	std::cout << "Time: " << (double)clock() / CLOCKS_PER_SEC << "\n";
 	return 0;
 }
 
