bfb02e7f984f7ebfbc823820d0210dfcb7ab5341
Keller Brandt <103919048+KellerBrandt@users.noreply.github.com>
2024-11-21 21:43:17 -0800

    blackjack

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -5,6 +5,53 @@
 #include <unordered_map>
 #include <vector>
 
+class Node {
+  public:
+	std::vector<double> strategySum;
+	std::vector<double> strategy;
+	std::vector<double> regretSum;
+    bool isDefault;
+	// may be useless
+	std::vector<int> player;
+	int dealer;
+
+	Node(std::vector<int> player, int dealer) : player(player), dealer(dealer) {
+		strategySum = {0, 0};
+		strategy = {1.0 / 2.0, 1.0 / 2.0};
+		regretSum = {0, 0};
+        isDefault = false;
+	}
+
+    Node() {
+        isDefault = true;
+    }
+
+	// check later
+	void updateStrategy() {
+		for (int i = 0; i < 2; ++i) {
+			strategySum[i] += strategy[i];
+		}
+
+		double total = 0;
+		for (double i : regretSum) {
+			if (i > 0) {
+				total += i;
+			}
+		}
+		if (total == 0) {
+			strategy = {1.0 / 2.0, 1.0 / 2.0};
+		} else {
+			for (int i = 0; i < 2; ++i) {
+				if (regretSum[i] > 0) {
+					strategy[i] = regretSum[i] / total;
+				} else {
+					strategy[i] = 0;
+				}
+			}
+		}
+	}
+};
+
 /*
 Clubs, Diamonds, Hearts, Spades
 0-12   13-25     26-38   39-51
@@ -18,53 +65,74 @@ int cardsLeft = 52;
 */
 int actions[2] = {0, 1};
 
-std::unordered_map<int, std::vector<int>> nodes;
+std::unordered_map<int, Node> nodes;
 
-//possible idea: abstract out the suits and make just an int[] with the counts of each rank
+int primes[13] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43};
+int suits[13] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+
+// possible idea: abstract out the suits and make just an int[] with the counts of each rank
 double cfr() {
 	int deckSize = 52;
-    double total = 6497400; // 52 * 51 * 50 * 49
+	double total = 6497400; // 52 * 51 * 50 * 49
 
-    for (int i = 0; i < deckSize; ++i) {
-        for (int j = 0; j < deckSize; ++i) {
-            if (j == i) {
-                continue;
-            }
-            for (int a = 0; a < deckSize; ++a) {
-                if (a == i || a == j) {
-                    continue;
-                }
-                for (int b = 0; b < deckSize; ++b) {
-                    if (b == i || b == j || b == a) {
-                        continue;
-                    }
-                    cfr(std::vector<int>{i, j, a, b}, std::vector<int>{i, j}, std::vector<int>{a, b}, 1.0 / total, 1.0);
-                }
-            }
-        }
-    }
+	for (int i = 0; i < deckSize; ++i) {
+		for (int j = 0; j < deckSize; ++i) {
+			if (j == i) {
+				continue;
+			}
+			for (int a = 0; a < deckSize; ++a) {
+				if (a == i || a == j) {
+					continue;
+				}
+				for (int b = 0; b < deckSize; ++b) {
+					if (b == i || b == j || b == a) {
+						continue;
+					}
+					// possible error source, reducing cards to their suits
+					cfr(std::vector<int>{i, j, a, b}, std::vector<int>{i % 13, j % 13}, std::vector<int>{a % 13, b % 13}, 1.0 / total, 1.0);
+				}
+			}
+		}
+	}
 }
 
 /*
 the deck of cards, the players cards, the dealers cards
 */
 double cfr(std::vector<int> usedCards, std::vector<int> player, std::vector<int> dealer, double chance, double playerPercent) {
-
-}
-
-void makeDeck(int (&deck)[]) {
-	for (int i = 0; i < 52; ++i) {
-		deck[i] = i;
-	}
-	std::random_shuffle(&deck[0], &deck[51]);
-}
-
-std::vector<int> makeDeck(int deckSize) {
-    std::vector<int> deck;
-    for (int i = 0; i < deckSize; ++i) {
-        deck.push_back(i);
+    if (isTerminal(player)) {
+        return getReward(player, dealer);
     }
-    return deck;
+    int key = getKey(player, dealer[0]);
+    Node &node = nodes[key]; //dealer 0 is the revealed card
+    if (node.isDefault) {
+        nodes[key] = Node(player, dealer[0]);
+    }
+
+    
+}
+
+// possible error source
+int getKey(std::vector<int> cards, int dealer) {
+	int key = 1;
+	key *= primes[dealer % 13];
+	for (int i : cards) {
+		key *= primes[i % 13];
+	}
+	return key;
+}
+
+bool isTerminal(std::vector<int> player) {
+    int sum = 0;
+    int aceCount = 0;
+    for (int i : player) {
+        sum += suits[i];
+        aceCount += (i == 12);
+    }
+}
+
+double getReward(std::vector<int> player, std::vector<int> dealer) {
+
 }
 
 double train(int iterations) {
